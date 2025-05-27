f46b22ccd7193746484ba695fb0de94658a9065f
Paco-Humbert <humbert.paco@gmail.com>
2025-05-27 14:27:02 +0200

    Refactor : accueil.json

--- FrontEnd/assets/JS/accueil.js
+++ FrontEnd/assets/JS/accueil.js
@@ -1,26 +1,34 @@
 const gallery = document.querySelector(".gallery");                   // HTML gallery
-
+// Fonction asynchrone pour récupérer les travaux depuis l'API.
 async function getWorks() { 
-  try {                                                               // utiliser pour sécuriser si Fetch ne fonctionne pas
-    const response = await fetch("http://localhost:5678/api/works");  // requête HTTP get - requête GET swagger 
+// Utiliser pour sécuriser si Fetch ne fonctionne pas    
+  try {       
+// Requête API                                                            
+    const response = await fetch("http://localhost:5678/api/works");  
+// Vérification validité réponse
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const works = await response.json();                              // obtention tableau d'objets works ( chaque projet)
+// Obtention tableau d'objets works ( chaque projet)    
+    const works = await response.json();                              
     displayWorks(works);
-  } catch (error) {                                                   // si erreur dans le bloc "try" renvoi directement ici + affichage erreur dans console
+// Si erreur dans le bloc "try" renvoi directement ici + affichage erreur dans console    
+  } catch (error) {                                                   
     console.error("Erreur lors du chargement des travaux :", error);
   }
 }
-
-function displayWorks(works) {                                        // affiche les projets dans le HTML
-  gallery.innerHTML = "";                                             // vide la galerie d’abord
-  works.forEach(work => {                                             // boucle pour chaque objet work
-    const figure = document.createElement("figure");                  // création balise 
-
-    const img = document.createElement("img");                        // création balise 
+// Affiche les projets dans le HTML
+function displayWorks(works) {  
+// Vide la galerie d’abord                                          
+  gallery.innerHTML = "";
+// Boucle pour chaque objet work                                               
+  works.forEach(work => {
+// Création balise                                                 
+    const figure = document.createElement("figure");                   
+// Création balise 
+    const img = document.createElement("img");                        
     img.src = work.imageUrl;
     img.alt = work.title;
-
-    const caption = document.createElement("figcaption");             // création balise 
+// Création balise
+    const caption = document.createElement("figcaption");              
     caption.textContent = work.title;
 
     figure.appendChild(img);
@@ -29,4 +37,5 @@ function displayWorks(works) {                                        // affiche
   });
 }
 
-getWorks();                                                           // lance la récupération dès le chargement de la page
+// Lance la récupération dès le chargement de la page
+getWorks();                                                           
