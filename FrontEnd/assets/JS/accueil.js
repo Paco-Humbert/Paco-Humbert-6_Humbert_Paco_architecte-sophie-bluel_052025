d9192f3bc167b4f0214839e5b3e215b4dee29d0a
Paco-Humbert <humbert.paco@gmail.com>
2025-06-02 13:04:17 +0200

    feat : récupération des projets depuis API enter affichage dans la galerie

--- FrontEnd/assets/JS/accueil.js
+++ FrontEnd/assets/JS/accueil.js
@@ -1,3 +1,27 @@
+// sélection élément HTML qui va contenir tous les projets
 const gallery = document.querySelector(".gallery");
+// sélection conteneur Filtres ( Boutons)
 const filtersContainer = document.querySelector("#filters");
+// Variable stockant les projets récupérés depuis l'API
 let allWorks = [];
+
+// Récupération des travaux depuis API
+async function getWorks() {
+  try {
+// Appel API    
+    const response = await fetch ("http://localhost:5678/api/works");
+// Vérification réponse valide, sinon affichage message d'erreur   
+    if (!response.ok) throw new Error ('`HTTP error! status: ${response.status}');
+// Transforme la réponse JSON en tableau
+    const works = await response.json();
+// Stockage projets dans variable globale
+    allWorks = works;
+// Affichage des Projets
+    displayWorks (works);
+// génèration des filtres à partir des catégéories présentes 
+    generateFiltersFromWorks(works);
+  } catch (error) {
+// En cas d'erreur affichage message d'erreur
+  console.error("Error Loading works.", error)    
+  }
+}
