84b1ffec5e9de45df9359269e501e4fbdd209a93
Paco-Humbert <humbert.paco@gmail.com>
2025-06-04 21:14:48 +0200

    refactor : html, Css for display

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -92,9 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 logOut.textContent = "logout";
 
                 logOut.addEventListener("click", (e) => {
-                    // Empêche le comportement par défaut 
-                    e.preventDefault(); 
-
+                
                     // Supprime le token 
                     localStorage.removeItem("token");
 
